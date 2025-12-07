228386159410c3fc2cccfff25a8781fa659e4659
HSK-DunDunRST <gon5073@naver.com>
2025-12-07 21:27:34 +0900

    SET - 프로젝트 생성

---
+++ src/main/java/com/hsk/cafeteria/LocalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hsk.cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsk/cafeteria/LocalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsk.cafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
